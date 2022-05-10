485cd8c52aae3f8dea7421748271e088dff97f23
hetal149 <72338210+hetal149@users.noreply.github.com>
2022-05-10 14:03:25 +0530

    Angular Ticket System

---
+++ AngularTicketSystem/frontend/src/app/header/header.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './_services/auth.service';
+import { Router } from '@angular/router';
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  // constructor() { }
+
+  // ngOnInit(): void {
+  // }
+
+  public totalItem : number = 0;
+  public searchTerm !: string;
+  // public isLoggedIn: boolean = false;
+  constructor(public AuthService:AuthService,public router: Router) {this.AuthService.user
+    if(this.AuthService.loggedIn()){
+      this.AuthService.isloggedin= true;
+      this.router.navigate(['home'])
+
+    }
+  }
+
+  ngOnInit(): void {
+   
+  }
+
+  onLogoutClick() {
+    // this.cartService.removeAllCart();
+    this.AuthService.logout();
+    this.AuthService.isloggedin = false;
+    this.router.navigate(['/home']);
+   
+  }
+
+}
